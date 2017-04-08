2587de1cb7f9737a2a31f7a008c2e5a067ad8806
Andrey Antukh <niwi@niwi.nz>
2017-04-08 14:11:19 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject funcool/rumext "1.0.0"
+  :description "A collection of macros and helpers for rum (used in uxbox)."
+  :url "https://github.com/funcool/rumext"
+
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.495" :scope "provided"]]
+
+  :deploy-repositories {"releases" :clojars
+                        "snapshots" :clojars}
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :jar-exclusions [#"\.swp|\.swo|user.clj"])
+
---
+++ src/rumext/core.cljs
@@ -0,0 +1,41 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
+(ns rumext
+  (:require [sablono.core :refer-macros [html]]
+            [sablono.server :as server]
+            [rum.core :as rum]
+            [lentes.core :as l]
+            [goog.dom.forms :as gforms]))
+
+(extend-type cljs.core.UUID
+  INamed
+  (-name [this] (str this))
+  (-namespace [_] ""))
+
+(defn local
+  ([]
+   (rum/local {} :rum/local))
+  ([initial]
+   (rum/local initial :rum/local))
+  ([key initial]
+   (rum/local initial key)))
+
+(defn lazy-component
+  [builder render mixins display-name]
+  (let [ctor (delay (builder render mixins display-name))]
+    (fn [& args]
+      (apply @ctor args))))
+
+(def mount rum/mount)
+(def static rum/static)
+(def ref-node rum/ref-node)
+(def dom-node rum/dom-node)
+(def react rum/react)
+(def reactive rum/reactive)
+(def with-key rum/with-key)
+(def render-html server/render)
+(def render-static-html server/render-static)
