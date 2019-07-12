442e6365aea8b35aab3f9b6ffbfb91d4d3ecf27a
Andrey Antukh <niwi@niwi.nz>
2019-07-12 12:58:51 +0200

    WIP

--- examples/rumext/examples/timer_static.cljs
+++ examples/rumext/examples/timer_static.cljs
@@ -1,29 +1,9 @@
 (ns rumext.examples.timer-static
-  (:require [rumext.core :as rmx :refer-macros [defc]]
+  (:require [rumext.core :as rmx]
             [rumext.examples.util :as util]))
 
-(defc timer-static
-  ;; {:mixins [rmx/reactive]
-  ;;  :init (fn [state]
-  ;;          (prn "init" state)
-  ;;          state)
-  ;;  :derive-state (fn [state]
-  ;;                  (prn "derive-state")
-  ;;                  state)
-  ;;  :did-mount (fn [state]
-  ;;               (prn "did-mount")
-  ;;               state)
-  ;;  :should-update (fn [lstate nstate]
-  ;;                   (prn "should-update" lstate nstate)
-  ;;                   true)
-  ;;  :make-snapshot (fn [state]
-  ;;                   (prn "make-snapshot")
-  ;;                   "foobar")
-  ;;  :did-update (fn [state snapshot]
-  ;;                (prn "did-update" snapshot)
-  ;;                state)}
+(rmx/defc timer-static
   [label ts]
-  ;; (prn "render")
   [:div label ": "
    [:span {:style {:color @util/*color}} (util/format-time ts)]])
 
@@ -31,5 +11,4 @@
   (rmx/mount (timer-static "Static" @util/*clock) el)
   (add-watch util/*clock :timer-static
              (fn [_ _ _ new-val]
-               ;; (prn "****")
                (rmx/mount (timer-static "Static" new-val) el))))
