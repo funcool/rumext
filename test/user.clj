c1fdf365c4ab7637a9cdd2ee4383b9ce24c45894
Andrey Antukh <niwi@niwi.nz>
2022-01-20 18:01:56 +0100

    Add user.clj namespace.

---
+++ test/user.clj
@@ -0,0 +1,7 @@
+(ns user
+  (:require
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl :as repl]
+   [clojure.walk :refer [macroexpand-all]]
+   [clojure.pprint :refer [pprint]]
+   [clojure.repl :refer :all]))
